15190d2dd90631779ecebbcf75e35f7faac24750
Anu12346 <annapoornahp97@gmail.com>
2024-06-16 22:03:54 +0530

    Updated Files

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,7 +5,7 @@ export const Cart = () => {
   useTitle("Cart");
   
   const products = [
-    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png",}, 
+    {id: 1, "name": "Sony Wh-Ch510 Bluetooth Wireless1", "price": 149, "image": "/assets/images/1001.png",}, 
     {id: 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png",}, 
   ]
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ export const Home = () => {
   useTitle("Home");
 
   const products = [
-    {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png"},
+    {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless1", "price": 149, "image": "/assets/images/1001.png"},
     {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png"},
     {"id": 3, "name": "JBL Tune 760NC", "price": 179, "image": "/assets/images/1003.png"},
     {"id": 4, "name": "Logitech H111 Wired", "price": 39, "image": "/assets/images/1004.png"},
